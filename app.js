c089e5520f13389bdbd26be9f69ada077ab6bb0f
YenYooo <point211@gmail.com>
2024-08-28 17:03:20 +0800

    build: use express-handlebars as view engine & modify json for method

--- app.js
+++ app.js
@@ -1,30 +1,33 @@
-// 載入 Express
-const express = require('express')
-const app = express()
-// 設定連接埠
-const port = 3000
+const express = require("express"); // 載入 Express
+const { engine } = require("express-handlebars"); // 載入 Express-handlebars
+const app = express();
+const port = 3000; // 設定連接埠
+
+// 告訴 Express 把樣本引擎交給 Express-handlebars
+app.engine('.hbs', engine({ extname: '.hbs'}))
+app.set('view engine', '.hbs')
+app.set('views', './views')
 
 // 將 restaurant.json 載入 Express 中
-app.subscribe(express.static('public'))
+app.use(express.static("public"));
 
 // 將網站首頁從根目錄(/)重定位到餐廳清單(/restaurants)
-app.get('/', (req, res) => {
-  res.redirect('/restaurants')
-})
+app.get("/", (req, res) => {
+  res.redirect("/restaurants");
+});
 
-// 設定首頁路由
-app.get('/restaurants', (req, res) => {
-  res.send('listing restaurants')
-})
+// 設定首頁路由 -> 修改成渲染 index.hbs 的內容
+app.get("/restaurants", (req, res) => {
+  res.render("index");
+});
 
 // 用 params 做動態路由
-app.get('/restaurant/:id', (req, res) => {
-  const id = req.params.id
-  res.send(`read restaurant: ${id}`)
-})
-
+app.get("/restaurant/:id", (req, res) => {
+  const id = req.params.id;
+  res.send(`read restaurant: ${id}`);
+});
 
 // 啟動伺服器
 app.listen(port, () => {
   console.log(`express server is running on http://localhost:${port}`);
-})
+});
